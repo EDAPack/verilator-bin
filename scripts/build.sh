5dcd814c506186b13f01a796f039d5df91260f89
Matthew Ballance <matt.ballance@gmail.com>
2026-01-03 01:37:43 +0000

    Add Windows builds (MinGW and Cygwin) with pacman packages
    - Add build-windows-mingw job with win32/win64 builds
    - Add build-windows-cygwin job with 32/64-bit builds
    - Create .tar.gz releases for all Windows builds
    - Add pacman package creation for MinGW64
    - Include runtime DLLs in Windows packages
    - Update build.sh to handle MinGW/Cygwin environments

--- scripts/build.sh
+++ scripts/build.sh
@@ -37,8 +37,36 @@ elif test $(uname -s) = "Darwin"; then
     REMOVE_PREGEN=1
     
     rls_plat=${image}
-elif test $(uname -s) = "Windows"; then
-    rls_plat="windows-x64"
+elif echo $(uname -s) | grep -q "MINGW\|MSYS"; then
+    # MinGW environment
+    if test -z $image; then
+        if test "$(uname -m)" = "x86_64"; then
+            image=mingw64
+        else
+            image=mingw32
+        fi
+    fi
+    
+    # Ensure python is available
+    which python3 || alias python3=python
+    
+    rls_plat=${image}
+    IS_WINDOWS=1
+elif echo $(uname -s) | grep -q "CYGWIN"; then
+    # Cygwin environment
+    if test -z $image; then
+        if test "$(uname -m)" = "x86_64"; then
+            image=cygwin64
+        else
+            image=cygwin32
+        fi
+    fi
+    
+    # Install meson and ninja if not present
+    pip3 install meson ninja || python3 -m pip install meson ninja
+    
+    rls_plat=${image}
+    IS_WINDOWS=1
 fi
 
 #********************************************************************
@@ -110,6 +138,30 @@ if test $? -ne 0; then exit 1; fi
 #* Create release tarball
 #********************************************************************
 cd ${root}/release
+
+# For Windows builds, we need to include the runtime libraries
+if test "x${IS_WINDOWS}" = "x1"; then
+    echo "Creating Windows release package with runtime libraries..."
+    
+    # Copy required DLLs for MinGW/Cygwin
+    if echo $(uname -s) | grep -q "MINGW\|MSYS"; then
+        # MinGW: Copy MinGW runtime DLLs
+        mkdir -p verilator/bin
+        cp -v /mingw*/bin/libgcc_s_*.dll verilator/bin/ 2>/dev/null || true
+        cp -v /mingw*/bin/libstdc++-*.dll verilator/bin/ 2>/dev/null || true
+        cp -v /mingw*/bin/libwinpthread-*.dll verilator/bin/ 2>/dev/null || true
+        cp -v /mingw*/bin/libgmp-*.dll verilator/bin/ 2>/dev/null || true
+        cp -v /mingw*/bin/libmpfr-*.dll verilator/bin/ 2>/dev/null || true
+    elif echo $(uname -s) | grep -q "CYGWIN"; then
+        # Cygwin: Dependencies are in /usr/bin
+        mkdir -p verilator/bin
+        cp -v /usr/bin/cyggcc_s-*.dll verilator/bin/ 2>/dev/null || true
+        cp -v /usr/bin/cygstdc++-*.dll verilator/bin/ 2>/dev/null || true
+        cp -v /usr/bin/cygwin1.dll verilator/bin/ 2>/dev/null || true
+        cp -v /usr/bin/cyggmp-*.dll verilator/bin/ 2>/dev/null || true
+        cp -v /usr/bin/cygmpfr-*.dll verilator/bin/ 2>/dev/null || true
+    fi
+fi
 
 tar czf verilator-${rls_plat}-${rls_version}.tar.gz verilator
 if test $? -ne 0; then exit 1; fi
